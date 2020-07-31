0e5d517f8b5c4468b65e880e3f704515e9a130e8
shui <2386686364@qq.com>
2020-07-31 17:56:41 +0800

    11

---
+++ maven-vue/one/src/main/java/com/shui/one/config/MybatisPlusConfig.java
@@ -0,0 +1,22 @@
+package com.shui.one.config;
+
+import com.baomidou.mybatisplus.extension.plugins.PaginationInterceptor;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+/**
+ * 分页插件
+ */
+
+@Configuration
+@EnableTransactionManagement
+//@MapperScan("com.markerhub.mapper")
+public class MybatisPlusConfig {
+    @Bean
+    public PaginationInterceptor paginationInterceptor() {
+        PaginationInterceptor paginationInterceptor = new PaginationInterceptor();
+        return paginationInterceptor;
+    }
+}
