17dc6615c4a904cbae0af060d44245104617d1a1
shui <2386686364@qq.com>
2020-08-03 17:34:23 +0800

    整合shiro，并用jwt进行身份校验。

---
+++ maven-vue/one/src/main/java/com/shui/one/common/shiro/ShiroUtil.java
@@ -0,0 +1,11 @@
+package com.shui.one.common.shiro;
+
+import org.apache.shiro.SecurityUtils;
+
+public class ShiroUtil {
+
+    public static AccountProfile getProfile() {
+        return (AccountProfile) SecurityUtils.getSubject().getPrincipal();
+    }
+
+}
