bc264d42055eb1ae94c568b8c5ee3176cfe1cbe7
shui <2386686364@qq.com>
2020-08-03 17:46:14 +0800

    拦截器： 跨域

---
+++ maven-vue/one/src/main/java/com/shui/one/common/WebMvcConfig.java
@@ -0,0 +1,20 @@
+package com.shui.one.common;
+
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+public class WebMvcConfig implements WebMvcConfigurer {
+
+    /**
+     * 跨域问题
+     */
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("*")
+                .allowedMethods("GET", "HEAD", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowCredentials(true)
+                .maxAge(3600)
+                .allowedHeaders("*");
+    }
+}
